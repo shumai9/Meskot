32122d113cd4a6ccfde310b1bfdbe27c02db4ad9
Shumai <shumai9@github.com>
2018-09-14 23:46:18 +0100

    added routes

--- src/App.js
+++ src/App.js
@@ -1,31 +1,45 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Link, Route, Switch} from 'react-router-dom'
 import logo from './shuma.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
+      <Router>
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
             <div className="nav">
             <ul>
-              <li>HOME</li>
-              <li>ABOUT</li>
-              <li>TIMELINE</li>
-              <li>EDUCATION</li>
-              <li>EXPERIENCE</li>
-              <li>PROJECTS</li>
-              <li>BLOG</li>
-              <li>CONTACT</li>
+              <li><Link to={"/"}>HOME</Link></li>
+              <li><Link to={"/about"}>ABOUT</Link></li>
+              <li><Link to={"/timeline"}>TIMELINE</Link></li>
+              <li><Link to={"/education"}>EDUCATION</Link></li>
+              <li><Link to={"/experience"}>EXPERIENCE</Link></li>
+              <li><Link to={"/projects"}>PROJECTS</Link></li>
+              <li><Link to={"/blog"}>BLOG</Link></li>
+              <li><Link to={"/contact"}>CONTACT</Link></li>
             </ul>
             </div>
           <h1 className="App-title">Heya! I AM SHUMBAHRI </h1>
         </header>
-        <p className="App-intro">
-          Yay!!! You made it to my Web site Welcome
-          </p>
+        <div className="main route">
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="timeline" component={TIMELINE} />
+          <Route exact path="/education" component={EDUCATION} />
+          <Route exact path="/experience" component={EXPERIENCE} />
+          <Route exact path="projects" component={PROJECTS} />
+          <Route exact path="/blog" component={BLOG} />
+          <Route exact path="/contact" component={CONTACT} />
+
+
+          <p className="App-intro">Yay!!! You made it to my Web site Welcome</p>
+       
+        </div>
       </div>
+    </Router>
     );
   }
 }
