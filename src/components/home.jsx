8baa1a86b89a97c6eaed3ed571232727bbbf86e1
Shumai <shumai9@github.com>
2018-12-29 13:09:51 +0000

    Trello card:https://trello.com/c/YlGDH2eq added method to display data

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -7,15 +7,32 @@ class Home extends React.Component {
       loaded: false,
       bucket: ''
     };
+    this.writer = data => {
+      for (let i = 0; i < data.length; i++) {
+        return <p>{data[i]}</p>;
+      }
+    };
   }
 
   render() {
+    const title = this.props.data.intro;
+    const body = this.props.data.body;
+    const idea = this.props.data.catch;
+    const url = this.props.data.img;
     return (
       <div id="main" className="home">
-        <h1>{this.props.data.intro}</h1>
-        <p>{this.props.data.body}</p>
-        <p>{this.props.data.catch}</p>
-        <img src={this.props.data.img} alt="flora" />
+        {title.map((text, i) => {
+          return <h1 key={i}>{text}</h1>;
+        })}
+        {body.map((text, j) => {
+          return <p key={j}> {text}</p>;
+        })}
+        {url.map((text, l) => {
+          return <img src={url} key={l} alt={`${l}`} />;
+        })}
+        {idea.map((text, k) => {
+          return <p key={k}>{text}</p>;
+        })}
       </div>
     );
   }
