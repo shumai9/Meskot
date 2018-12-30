2caf882ec24ec896c095e8a63448f8f11ab127fc
Shumai <shumai9@github.com>
2018-12-30 13:13:39 +0000

    restructured json data and refactored projects

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,52 +1,32 @@
 import React from 'react';
 
 class Projects extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      summary: this.props.data.summary,
+      aim: this.props.data.aim,
+      time: this.props.data.time,
+      url: this.props.data.img
+    };
+  }
   render() {
     const title = this.props.data.title;
-    const aim = this.props.data.aim;
-    const time = this.props.data.time;
-    const summary = this.props.data.summary;
-    const url = this.props.data.img;
+
     return (
       <div id="main" className="projects menu">
         <h5>PROJECTS</h5>
         {title.map((text, i) => {
           return (
             <div key={i}>
-              <h3>{text}</h3>
+              <h6>{text}</h6>
+              <img id="proj" src={`${this.state.url[i]}`} alt={`${i}`} />
+              <p>{this.state.aim[i]}</p>
+              <p>{this.state.time[i]}</p>
+              <p>{this.state.summary[i]}</p>
             </div>
           );
         })}
-        {aim.map((text, i) => {
-          return (
-            <div key={i}>
-              <p>{text}</p>
-            </div>
-          );
-        })}
-        {time.map((text, i) => {
-          return (
-            <div key={i}>
-              <p>{text}</p>
-            </div>
-          );
-        })}
-        {summary.map((text, i) => {
-          return (
-            <div key={i}>
-              <p>{text}</p>
-            </div>
-          );
-        })}
-        <div className="img-container">
-          {url.map((link, i) => {
-            return (
-              <div key={i}>
-                <img id="proj" src={link} alt={`${i}`} />
-              </div>
-            );
-          })}
-        </div>
       </div>
     );
   }
