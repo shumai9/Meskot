a764fb6f40c4a963359cbc4a1e7cfe0040e993fc
Shumai <shumai9@github.com>
2018-12-30 14:36:03 +0000

    refactored projects and edited timeline

--- src/components/timeline.jsx
+++ src/components/timeline.jsx
@@ -1,14 +1,34 @@
 import React from 'react';
 
 class Timeline extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      places: this.props.data.places,
+      time: this.props.data.date,
+      date: this.props.data.date,
+      url: this.props.data.img
+    };
+  }
   render() {
+    const job = this.props.data.career;
+    const place = this.props.data.places;
+
     return (
       <div id="main" className="timeline">
-        <p> It all started</p>
-        <li>{this.props.data.career}</li>
-        <li>
-          {this.props.data.date} : {this.props.data.place}
-        </li>
+        {job.map((title, k) => {
+          return <h1>{title}</h1>;
+        })}
+        {place.map((title, k) => {
+          return (
+            <div>
+              <img src={`${this.state.url[k]}`} alt={`${k}`} />
+              <p>
+                {this.state.places[k]} ; {this.state.time[k]}
+              </p>
+            </div>
+          );
+        })}
       </div>
     );
   }
